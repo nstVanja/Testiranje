61105d9e334696cab8d592b7dd1950fff0802db3
Vanja <32024620+nstVanja@users.noreply.github.com>
2022-07-09 00:13:36 +0200

    Delete script.js

--- mailto/js/script.js
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-  document.write("<a href=\"mail" + "to:" + new Array("your.name","example.net").join("@") + "\">" + new Array("your.name","example.net").join("@") + "</" + "a>");
-</script>
