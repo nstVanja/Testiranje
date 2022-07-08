24134505f0362d3c0b50958afef88fb68c24a583
Vanja <32024620+nstVanja@users.noreply.github.com>
2022-07-08 22:02:36 +0200

    Update script.js

---
+++ mailto/js/script.js
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+  document.write("<a href=\"mail" + "to:" + new Array("your.name","example.net").join("@") + "\">" + new Array("your.name","example.net").join("@") + "</" + "a>");
+</script>
